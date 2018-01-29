92a269127f6778de78b30b646620ad3d5b06044c
Nicole Eidi <nicoleeidi1@gmail.com>
2018-01-29 19:06:39 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -36,7 +36,9 @@ def now_serving(katz_deli)
     puts "There is nobody waiting to be served!"
   else
     puts "Currently serving " + katz_deli[0] + "."
-    array.shift 
+    array.shift
+  end
+end 
 
   #Welcome, Ada. You are number 1 in line.
 
