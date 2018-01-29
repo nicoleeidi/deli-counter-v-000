95b75d84a029dec7caf6363392e694255a334e34
Nicole Eidi <nicoleeidi1@gmail.com>
2018-01-29 18:13:58 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,2 +1,11 @@
 # Write your code here.
 katz_deli = []
+def line(katz_deli) 
+  if katz_deli.length=0 
+    "The line is currently empty."
+  else 
+    i=1
+    katz_deli.map do |name|
+      
+      
+  line #=> "The line is currently: 1. Ada 2. Grace 3. Kent"
