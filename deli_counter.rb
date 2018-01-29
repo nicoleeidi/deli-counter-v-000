9eda2e064ebe128effa1d03c4ef4135be16128cc
Nicole Eidi <nicoleeidi1@gmail.com>
2018-01-29 19:06:22 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -32,10 +32,11 @@ def take_a_number(katz_deli,name)
 end
 
 def now_serving(katz_deli)
-  if katz_deli.length == 0 
+  if katz_deli.length == 0
     puts "There is nobody waiting to be served!"
   else
-    puts "Currently serving "
+    puts "Currently serving " + katz_deli[0] + "."
+    array.shift 
 
   #Welcome, Ada. You are number 1 in line.
 
